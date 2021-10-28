fa1e27afbdd1d870ef13c8aa23e94e7fd697bb44
LucasLoFaro-BNA <84343645+LucasLoFaro-BNA@users.noreply.github.com>
2021-10-27 21:00:47 -0300

    Update UserInterface.cs

--- PracticaBatch0/UI/UserInterface.cs
+++ PracticaBatch0/UI/UserInterface.cs
@@ -1,5 +1,6 @@
 ﻿using PracticaBatch0.Models;
 using System;
+using System.Globalization;
 
 namespace PracticaBatch0.UI
 {
@@ -35,20 +36,10 @@ namespace PracticaBatch0.UI
 
             try
             {
-                DateTime Date = new DateTime(
-                                        Convert.ToInt32(input.Substring(0, 4)),                         //YYYY
-                                        Convert.ToInt32(input.Substring(4, 2)),                         //MM
-                                        Convert.ToInt32(input.Substring(6, 2)),                         //DD
-                                        Convert.ToInt32(input.Substring(8, 2)),                         //HH
-                                        Convert.ToInt32(input.Substring(10, 2)),                        //mm
-                                        Convert.ToInt32(input.Substring(12, 2)));                       //SS
-
+                DateTime Date = DateTime.ParseExact(input.Substring(0, 14), "yyyyMMddHHmmss", new CultureInfo("es-AR"));
                 Double Temperature = Convert.ToInt32(input.Substring(14, 3)) / 10;
-
                 Double Humidity = Convert.ToDouble(input.Substring(17, 3)) / 10;
-
                 String SensorID = input.Substring(20, 4);
-
                 Boolean SensorStatus = Convert.ToBoolean(Convert.ToInt32(input.Substring(24, 1)));
 
                 return new Record(Date, Temperature, Humidity, SensorID, SensorStatus);
